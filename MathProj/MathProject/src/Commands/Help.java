b053750b0e90aad40f3a730d38d71ee2004c8970
Drobysh Dmitry <90309201+NorthCapDiamond@users.noreply.github.com>
2023-01-13 01:06:48 +0300

    Update Help.java

--- MathProj/MathProject/src/Commands/Help.java
+++ MathProj/MathProject/src/Commands/Help.java
@@ -3,12 +3,12 @@ package Commands;
 import util.Environment;
 
 public class Help implements ICommand{
-    @Override
+    @@Override
     public void execute(Environment environment, String message) {
         environment.getPrintStream().println("\nhelp : Display help for available commands.\n" +
-                "info : returns main information about this work" +
+                "info : returns main information about this work\n" +
                 "add_one_by_one : add a new elements to the collection. ( you need to write dots one by one )\n" +
-                "integrate : Calculates the integral sums for some partition of the bar" +
+                "integrate : Calculates the integral sums for some partition of the bar\n" +
                 "clean : clear the collection.\n" +
                 "exit : exit the program. (without saving to file)\n" +
                 "remove_by_id id : Remove an element from the collection by its id.\n" +
